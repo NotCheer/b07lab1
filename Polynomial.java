6985cfa66dc4aafebf49b22e1c8560b76497af7a
NotCheer <nut.li@mail.utoronto.ca>
2023-09-18 16:30:05 -0400

    update Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,29 +1,59 @@
 import java.lang.Math;
 
 public class Polynomial{
-  int[] coefficients;
+  double[] coefficients;
 
   public Polynomial(){
   }
 
-  public Polynomial(int[] c){
+  public Polynomial(double[] c){
     coefficients = c;
   }
 
-  public void add(Polynomial p){
+  public Polynomial add(Polynomial p){
+    if (this == null) return null;
     int t = p.coefficients.length;
-    if(t>this.coefficients.length) t=this.coefficients.length;
+    int k;
+    double sum[];
+    if(t>this.coefficients.length) 
+    {
+      k=t;
+      t=this.coefficients.length;
+      sum = p.coefficients;
+    }
+    else 
+    {
+      k = this.coefficients.length;
+      sum = this.coefficients;
+    }
 
     for(int i=0; i<t; i++){
-      this.coefficients[i]+=p.coefficients[i];
+      sum[i]=this.coefficients[i]+p.coefficients[i];
     }
+
+    Polynomial re = new Polynomial(sum);
+    return re;
   }
-  public int evaluate(int a){
-    int result=0;
+  
+  public double evaluate(double a){
+    if (this==null || this.coefficients==null) return 0.0;
+    double result=0;
     for(int i=0; i<this.coefficients.length; i++)
     {
       result+=this.coefficients[i] * Math.pow(a,i);
     }
     return result;
+  }
+
+  public boolean hasRoot(double root)
+  {
+    if (this==null) return false;
+    double sum = 0;
+    for(int i=0; i<this.coefficients.length; i++)
+    {
+      sum += Math.pow(root, i)*this.coefficients[i];
+    }
+    if (sum == 0) return true;
+    else return false;
   }
 }
