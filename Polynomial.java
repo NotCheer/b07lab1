3331b2b33dcdd338b7d74d5bf1c489f54d950424
NotCheer <nut.li@mail.utoronto.ca>
2023-10-01 21:05:02 -0400

    update Driver.java and Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,4 +1,9 @@
 import java.lang.Math;
+import java.io.PrintStream;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Polynomial{
   double[] coefficients;
@@ -12,13 +17,57 @@ public class Polynomial{
 		exponents = e;
   }
 
+  public Polynomial(File file) throws IOException
+  {
+    Scanner s = new Scanner(file);
+    String line = s.nextLine();
+    
+    int i=1, index=0, pin=0;
+
+    String[] tmp = new String[2];
+    String elements[] = line.split("[+-]+");
+    double[] coef = new double[elements.length];
+    int[] expo = new int[elements.length];
+    boolean flag = true;
+    for(int n=0; n<elements[0].length(); n++)
+    {
+      if(elements[0].substring(n,n+1)=="x")
+      {
+        flag = false;
+        i=0;
+        break;
+      }
+    }
+    if(flag)
+    {
+      coef[0]=Double.parseDouble(elements[0]);
+      if(line.substring(0,1)=="-")
+      {
+        coef[0]*=-1;
+        pin++;
+      }
+      pin+=elements[0].length();
+      expo[0]=0;
+    }
+    for(i=i; i<elements.length; i++)
+    {
+      tmp=elements[i].split("x");
+      coef[i]=Double.parseDouble(tmp[0]);
+      if(line.substring(pin,pin+1)=="-") coef[i]*=-1;
+      expo[i]=Integer.parseInt(tmp[1]);
+      pin+=(1+elements[i].length());
+    }
+    this.coefficients=coef;
+    this.exponents=expo;
+  }
+
   public Polynomial add(Polynomial p){
     if (this == null) return null;
     int pLength = p.coefficients.length;
     int thisLength = this.coefficients.length;
-		k = max(pLength, thisLength);
-		double newPoly[k];
-		int newExpo[k];
+		int k = Math.max(pLength, thisLength);
+		double[] newPoly = new double[k];
+		int[] newExpo = new int[k];
 
 		int i=0, j=0, index=0;
     while(i<thisLength || j<pLength){
@@ -30,7 +79,7 @@ public class Polynomial{
 				i++;
 				j++;
 			}
-			else if(j=pLength || this.exponents[i]<p.exponents[j])
+			else if(j==pLength || this.exponents[i]<p.exponents[j])
 			{
 				newExpo[index]=this.exponents[i];
 				newPoly[index]=this.coefficients[i];
@@ -45,9 +94,15 @@ public class Polynomial{
 				j++;
 			}
 		}
-		
+		double[] rePoly = new double[index];
+    int[] reExpo = new int[index];
+    for(int op=0; op<index; op++)
+    {
+      rePoly[op]=newPoly[op];
+      reExpo[op]=reExpo[op];
+    }
 
-    Polynomial re = new Polynomial(newPoly[index], newExpo[index]);
+    Polynomial re = new Polynomial(rePoly, reExpo);
     return re;
   }
   
@@ -72,17 +127,57 @@ public class Polynomial{
 	{
 		int pLength = p.exponents.length;
 		int thisLength = this.exponents.length;
-		int maxLength = p.exponents[pLength-1]*this.exponents[thisLength-1];
+		int maxLength = p.exponents[pLength-1]+this.exponents[thisLength-1];
 		int i=0, j=0, index=0;
 		int newExpo[] = new int[maxLength];
 		double newPoly[] = new double[maxLength];
+    int returnLength = 0;
 		while(i<thisLength || j<pLength)
 		{
-			if(this.exponents[i]=p.exponents[j])
+			if(i<thisLength && j<pLength && this.exponents[i]==p.exponents[j])
 			{
-				newPoly[this.exponents[i]*p.exponents[j]]=this.coefficients[i]*p.coefficients[j];
+				newPoly[this.exponents[i]+p.exponents[j]]+=this.coefficients[i]*p.coefficients[j];
 				i++;
 				j++;
-			}	
-		}	
+			}
+      else if(i<thisLength && this.exponents[i]<p.exponents[j])
+      {
+        newPoly[this.exponents[i]]+=this.coefficients[i];
+        i++;
+      }
+      else
+      {
+        newPoly[p.exponents[j]]+=p.coefficients[j];
+        j++;
+      }
+		}
+    for(int n=0; n<maxLength; n++) if(newPoly[n]!=0)
+    {
+      newPoly[returnLength]=newPoly[n];
+      newExpo[returnLength]=n;
+      returnLength++;
+    }
+    int[] reExpo = new int[returnLength];
+    double[] rePoly = new double[returnLength];
+    for(int n=0; n<returnLength; n++)
+    {
+      reExpo[n]=newExpo[n];
+      rePoly[n]=newPoly[n];
+    }
+    return new Polynomial(rePoly, reExpo);
+  }
+
+  public void saveToFile(String fileName) throws IOException
+  {
+    String out="";
+    for(int i=0; i<this.coefficients.length; i++)
+    {
+      out+=this.coefficients[i];
+      if(this.exponents[i]!=0) out+="x";
+      out+=this.exponents[i];
+    }
+    PrintStream ps = new PrintStream("./output.txt");
+    ps.println(out);
+    return;
+  }
 }
